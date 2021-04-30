79f0ca21f8e7f66bcade99ff0d8a952375e38e33
Hannah M <hannahemalm616@gmail.com>
2021-04-30 16:45:20 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string :location 
+      t.string :costume_inventory
+      t.integer :number_of_employees
+      t.boolean :open_for_business?
+      t.datetime :opening_time
+      t.datetime :closing_time
+    end 
+end 
+end 
